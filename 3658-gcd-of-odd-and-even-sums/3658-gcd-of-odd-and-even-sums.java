88433e69e0c3c13e12b541b42538ab7c27ea5aeb
Sumant Kumar <sumant.23bec10049@vitbhopal.ac.in>
2026-07-15 21:39:26 +0530

    Time: 573 ms (11.92%), Space: 42.7 MB (20.94%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,19 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n;
+        
+        int sumOdd = 0;
+        int sumEven = 0;
+
+        for (int i = 1; i<= n; i++) {
+            sumOdd += (2 * i -1);
+            sumEven += (2 * i);
+        }
+        for (int d = Math.min(sumOdd, sumEven); d >= 1; d-- ){
+            if (sumOdd % d == 0 && sumEven % d ==0){
+                return d;
+            }
+        }
+
+        return 1;
     }
 }
